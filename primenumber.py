94aae9787cadb84630e8e18f294dacb0670e4c8d
Jaewook Oh <kyle.oh95@gmail.com>
2019-12-30 13:56:43 +0900

    primenumberv1

--- primenumber.py
+++ primenumber.py
@@ -1,1 +1,15 @@
+# Version 1
+def isPrime(n):
+    if n <= 1:
+        return False
+    elif n % 2 == 0:
+        return False
+    else:
+        for i in range(3, n):
+            if n % i == 0:
+                return False
+        return True
+
+
 N = int(input())
+print(isPrime(N))
